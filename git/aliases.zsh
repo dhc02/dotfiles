11908c133df050064d51dd34dbf4d6e46d8adba5
Donnie Clapp <github@donnieclapp.com>
2016-09-15 16:24:47 -0500

    add a couple of git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,6 +6,8 @@ then
   alias git=$hub_path
 fi
 
+# REMINDER: aliases using git [alias] are in gitconfig.symlink
+
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -21,3 +23,7 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias wtf="git wtf" # runs dotfiles/bin/git-wtf
+
+# make git stash more friendly
+alias stash="git stash -u" # stash all files, tracked and untracked
