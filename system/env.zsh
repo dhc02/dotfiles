0684c392337a902d6bfb1e8e01ae1d1b926c8b4f
Donnie Clapp <dhclapp@gmail.com>
2018-03-29 12:47:25 -0500

    added a couple of aliases and scripts from http://furbo.org/2014/09/03/the-terminal/

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,9 @@
 export EDITOR='code'
+
+# by default, search through command history with whatever has been
+# typed at the prompt so far when the up and down keys are pressed
+
+bindkey '\e[A' history-search-backward
+bindkey '\e[B' history-search-forward
+
+export XCODE="`xcode-select --print-path`"
