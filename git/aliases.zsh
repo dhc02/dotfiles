a5882cd66c077b45897afbc18b6355385defff57
Donnie Clapp <dhclapp@gmail.com>
2020-02-05 14:38:48 -0600

    add git pushall alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,19 +11,21 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gc='git commit'
-alias gca='git commit -a'
+alias gca='git add . && git commit'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias gcam='git add . && git commit -m'
 alias wtf="git wtf" # runs dotfiles/bin/git-wtf
+
+alias pushall='git remote | xargs -L1 git push --all'
 
 # make git stash more friendly
 alias stash="git stash -u" # stash all files, tracked and untracked
