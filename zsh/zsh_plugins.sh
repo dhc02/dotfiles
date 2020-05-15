0561e80ebb49ce7b46bf9c4670dee37393b63d0f
Donnie Clapp <dhclapp@gmail.com>
2020-05-15 17:45:29 -0500

    add terminal-notifier to zsh-plugins

--- zsh/zsh_plugins.sh
+++ zsh/zsh_plugins.sh
@@ -1,31 +1,31 @@
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws/aws.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/asdf/asdf.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/asdf )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/mix-fast/mix-fast.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/mix-fast )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify/notify.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-autoenv/autoenv.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-autoenv )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-MikeDacre-SLASH-careful_rm/careful_rm.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-MikeDacre-SLASH-careful_rm )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gusaiani-SLASH-elixir-oh-my-zsh/elixir.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gusaiani-SLASH-elixir-oh-my-zsh )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-joow-SLASH-youtube-dl/youtube-dl.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-joow-SLASH-youtube-dl )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gangleri-SLASH-vue/vue.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gangleri-SLASH-vue )
-source /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z/zsh-z.plugin.zsh
-fpath+=( /Users/Donnie/Libary/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws/aws.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/asdf/asdf.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/asdf )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/mix-fast/mix-fast.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/mix-fast )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify/notify.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-autoenv/autoenv.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-autoenv )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-MikeDacre-SLASH-careful_rm/careful_rm.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-MikeDacre-SLASH-careful_rm )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gusaiani-SLASH-elixir-oh-my-zsh/elixir.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gusaiani-SLASH-elixir-oh-my-zsh )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-joow-SLASH-youtube-dl/youtube-dl.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-joow-SLASH-youtube-dl )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gangleri-SLASH-vue/vue.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gangleri-SLASH-vue )
+source /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z/zsh-z.plugin.zsh
+fpath+=( /Users/Donnie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z )
