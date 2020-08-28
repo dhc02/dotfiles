84361f170ab3fbf6a09d2880c4bc0ab88731d8c7
Donnie Clapp <dhclapp@gmail.com>
2020-08-28 16:22:26 -0500

    add openJDK (java) to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -4,3 +4,7 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # add Visual Studio Code "code" command
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+# add OpenJDK to path
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/openjdk/include"
